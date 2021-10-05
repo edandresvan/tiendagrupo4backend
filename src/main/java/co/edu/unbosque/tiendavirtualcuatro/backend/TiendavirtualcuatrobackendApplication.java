34096d04667b6d2ea79cea5f40677ca9f0b07f1b
Eder Andrés Ávila Niño <ederandresan@gmail.com>
2021-10-05 11:20:06 -0500

    Subiendo proyecto Spring Boot en blanco para el backend

---
+++ src/main/java/co/edu/unbosque/tiendavirtualcuatro/backend/TiendavirtualcuatrobackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.tiendavirtualcuatro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendavirtualcuatrobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendavirtualcuatrobackendApplication.class, args);
+	}
+
+}
